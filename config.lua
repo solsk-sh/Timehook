53addde163c9d921c074b42a17ba032a8c1f7389
solsk <hi@solsk.sh>
2023-04-10 00:26:16 -0700

    Workspace Cleanup
    Removes client.lua, cl_emergencyblips.lua and sv_emergencyblips.lua. Modifies fxmanifest.lua to reflect Timehook.

--- config.lua
+++ config.lua
@@ -3,6 +3,5 @@ Config = {
         ['👮 LSPD | '] = {887518674607562803, 2, nil},
         ['👮 Sheriff | '] = {887518674607562803, 17, nil},
         ['👮 SAHP | '] = {887518674607562803, 3, nil},
-    },
-    CLIENT_UPDATE_INTERVAL_SECONDS = 3, -- How frequently should the blips on the map update??
+    }
 }
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'JaredScar'
-description 'PoliceEMSActivity'
-version '2.0'
-url 'https://github.com/JaredScar/PoliceEMSActivity'
-
-client_scripts {
-	'client.lua',
-    'EmergencyBlips/cl_emergencyblips.lua',
-}
+author 'solsk'
+description 'Timehook'
+version '1.0'
+url 'https://github.com/solsk-sh/Timehook'
 
 server_scripts {
 	'config.lua',
 	"server.lua",
-    'EmergencyBlips/sv_emergencyblips.lua',
 }
